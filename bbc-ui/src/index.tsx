944b8fa528e5dc298b8089e0551326fe2366fff4
Pawel Szumiec <pawel.szumiec98@gmail.com>
2022-07-21 22:05:10 +0200

    removed boilerplate

--- bbc-ui/src/index.tsx
+++ bbc-ui/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
+import { ColorModeScript } from '@chakra-ui/react';
+
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import customTheme from './app/styles/customTheme';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,12 +14,10 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
       <App />
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
